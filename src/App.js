7d6d1b0426cdff590acf55f6b56cc10336762f9a
AravaSaiKrishna1911 <saikrishnaarava27@gmail.com>
2024-10-06 23:36:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Stocks from './components/Stocks';
+import RealEstate from './components/RealEstate';
+import MutualFunds from './components/MutualFunds';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/stocks" element={<Stocks />} />
+                    <Route path="/realEstate" element={<RealEstate />} />
+                    <Route path="/mutualFunds" element={<MutualFunds />} />
+                    <Route path="/logout" element={<div>Logout</div>} />
+                </Routes>
+                <Footer /> {/* Include the Footer here */}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
